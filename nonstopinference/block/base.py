d1c5b2ae9b891eedea14d30c64f554ba4a1b24da
pseudorm <chester.ph.cheng@gmail.com>
2022-06-03 22:05:14 +0800

    batch update

---
+++ nonstopinference/block/base.py
@@ -0,0 +1,15 @@
+class Block:
+    def __init__(self, ):
+        None
+    
+    def exception_handler(self):
+        NotImplementedError
+    
+    def fit(self):
+        NotImplementedError
+
+    def predict(self):
+        NotImplementedError
+
+    def transform(self):
+        NotImplementedError
---
+++ nonstopinference/exception/base.py
@@ -0,0 +1,71 @@
+import json
+import os
+import time
+import pickle
+
+
+class ExceptionRecord:
+    def __init__(self, data, data_id, block_id, status, error_msg):
+        self.data = data
+        self.data_id = data_id
+        self.block_id = block_id
+        self.status = status
+        self.error_message = error_msg
+
+
+class ExceptionLogger:
+    def __init__(self, name, log_dir=None, log_callback=None) -> None:
+        self.name = name
+        self.log_dir = log_dir
+        self.log_callback = log_callback
+        self._log_json, self._log_json_path = self._init_log_json()
+        self._log_pickle, self._log_pickle_path = self._init_log_pickle()
+
+    def _init_log_json(self):
+        json_name = self.name + "_" + int(time.now()) + ".json"
+        json_path = os.path.join(self.log_dir, json_name)
+
+        return open(json_path, "a"), json_path
+
+    def _init_log_pickle(self):
+        pickle_name = self.name + "_" + int(time.now()) + ".pkl"
+        pickle_path = os.path.join(self.log_dir, pickle_name)
+
+        return open(pickle_path, "a+"), pickle_path
+
+    def _log_to_json(self, exception_record):
+        error_body = {
+            "data_id": exception_record.data_id,
+            "block_id": exception_record.block_id,
+            "status": exception_record.status,
+            "error_message": exception_record.error_msg,
+        }
+
+        self._log_json.write(json.dumps(error_body), +"\n")
+
+    def _log_to_pickle(self, exception_record):
+        data_body = {
+            "data_id": exception_record.data_id,
+            "data": exception_record.data,
+        }
+        pickle.dump(data_body, self._log_pickle)
+
+    def log(
+        self, data, data_id: str, block_id: str, status: str, error_msg: str
+    ) -> None:
+        exception_record = ExceptionRecord(
+            data, data_id, block_id, status, error_msg
+        )
+
+        if self.log_callback:
+            self.log_callback(exception_record)
+
+        self._log_to_json(exception_record)
+        self._log_to_pickle(exception_record)
+
+
+class ExceptionReader:
+    def __init__(self, name, log_dir=None) -> None:
+        self.name = name
+        self.log_dir = log_dir
+        
