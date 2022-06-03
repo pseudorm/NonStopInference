798471c8193c56d68be94bc6393f08837ed991ce
pseudorm <chester.ph.cheng@gmail.com>
2022-06-03 13:24:19 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple ML inference pipeline that can handle exceptions gracefully',
+    author='Chester Cheng',
+    license='MIT',
+)
